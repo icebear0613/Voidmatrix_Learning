2c0777cd3be5f1aae399663bf92d50719e517be5
twx1284527 <tangjiangning@huawei.com>
2024-12-04 14:53:38 +0800

    TutorialZero_3 step 11

---
+++ TutorialZero_3/include/pea_bullet.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2024/12/4.
+//
+
+#ifndef GAME3_PEA_BULLET_H
+#define GAME3_PEA_BULLET_H
+
+#endif //GAME3_PEA_BULLET_H
---
+++ TutorialZero_3/include/sun_bullet.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2024/12/4.
+//
+
+#ifndef GAME3_SUN_BULLET_H
+#define GAME3_SUN_BULLET_H
+
+#endif //GAME3_SUN_BULLET_H
