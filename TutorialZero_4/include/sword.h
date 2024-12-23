cbbdd25347c1e86eaae74eef5adcb6d50fdf94bd
twx1284527 <tangjiangning@huawei.com>
2024-12-23 09:27:21 +0800

    TutorialZero 4 step 8: 黄蜂女角色类实现

---
+++ TutorialZero_4/include/sword.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "animation.h"
+#include "collision_box.h"
+
+class Sword
+{
+public:
+    Sword(const Vector2& position, bool move_left);
+    ~Sword();
+
+    void on_update(float delta);
+    void on_render();
+
+    bool check_valid() const
+    {
+        return is_valid;
+    }
+
+private:
+    const float SPEED_MOVE = 1250.0f;
+
+private:
+    Vector2 position;
+    Vector2 velocity;
+    Animation animation;
+    bool is_valid = true;
+    CollisionBox* collision_box = nullptr;
+};
+
